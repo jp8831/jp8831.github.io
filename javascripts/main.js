cdeafb70d487ff475dc96214599a38ed32d6763e
YoungJun Hong <akfls5655@gmail.com>
2017-12-03 03:16:30 +0900

    Make Demo Page
    Add content to demo page.
    Add working on paragraph to other content pages.
    
    Add style for WebGL canvas.
    Change height of header image in content stylesheet.
    
    Change media query for screen width  between 600px and 1000px to 600px and 1024px.
    
    Clear WebGL canvas color to black in default.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,18 @@
 function OnLoadBody ()
 {
-    
+    var glContext = null;
+    var canvas  = document.getElementById ("webgl-canvas");
+
+    glContext = canvas.getContext ("webgl");
+
+    if (!glContext)
+    {
+        alert("사용 중인 브라우저가 WebGL을 지원하지 않습니다.");
+        glContext = null;
+    }
+
+    glContext.clearColor (0.0, 0.0, 0.0, 1.0);
+    glContext.clear(glContext.COLOR_BUFFER_BIT | glContext.DEPTH_BUFFER_BIT);
 }
 
 function OnClickNavigationMenuIcon ()
