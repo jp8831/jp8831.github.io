3fd3d7acd5c07ea4f6f098a5559ef68fd6e3c5ea
YoungJun Hong <akfls5655@gmail.com>
2017-12-21 02:38:54 +0900

    Available to Hide Table of Contents

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,3 +2,8 @@ function OnClickNavigationMenuIcon ()
 {
     document.getElementById ("navigation-menu").classList.toggle ("show");
 }
+
+function OnClickTableOfContents ()
+{
+    document.getElementById ("table-of-contents").classList.toggle ("hide");
+}
