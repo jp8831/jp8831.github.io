4479fec388554e7ccd05adbd15f2d38f056701a4
YoungJun Hong <akfls5655@gmail.com>
2018-06-06 18:00:59 +0900

    쉐이더 업데이트

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,6 +1,7 @@
 attribute vec4 position;
-attribute vec4 normal;
+attribute vec3 normal;
 
+uniform mat4 world;
 uniform mat4 view;
 uniform mat4 projection;
 
@@ -8,10 +9,15 @@ varying highp vec3 diffuse;
 
 void main ()
 {
-    vec3 lightDir = normalize (vec3 (5.0, 5.0, 5.0));
-    vec3 normalizedNormal = vec3 (normalize (normal));
-    
-    diffuse = vec3 (dot (lightDir, normalizedNormal));
+    vec3 lightPos = vec3 (5.0, 5.0, -5.0);
 
-    gl_Position = projection * view * position;
+    vec3 lightDir = (world * position).xyz - lightPos;
+    lightDir = normalize (lightDir);
+
+    vec3 worldNormal = (world * vec4 (normal, 1.0)).xyz;
+    worldNormal = normalize (worldNormal);
+    
+    diffuse = vec3 (dot (-lightDir, worldNormal));
+
+    gl_Position = projection * view * world * position;
 }
